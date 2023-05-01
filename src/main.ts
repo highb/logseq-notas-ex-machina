ed00b3e2bb4137ea2c82218c5bb972820575bea2
Brandon <highb@users.noreply.github.com>
2023-04-30 21:11:43 -0700

    Fix plugin name

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import '@logseq/libs';
 async function main() {
-  logseq.UI.showMsg('Hello, LogSeq!');
+  logseq.UI.showMsg('Plugin started load: Hello, LogSeq!');
+
+  // When the /hello slash command is used, the following code will be executed
+  logseq.App.registerSlashCommand({
+    id: 'hello',
+    fn: async () => {
+      logseq.App.showMsg('Hello, LogSeq!');
+    }
+  });
 }
 logseq.ready(main).catch(console.error);
