d1636a67f20562cbe4a0d17db3f06755994150f9
Brandon <highb@users.noreply.github.com>
2023-04-30 21:40:05 -0700

    Add more slash commands, playing around

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,41 @@ import '@logseq/libs';
 async function main() {
   logseq.UI.showMsg('Plugin started load: Hello, LogSeq!');
 
-  // When the /hello slash command is used, the following code will be executed
-  logseq.App.registerSlashCommand({
-    id: 'hello',
-    fn: async () => {
-      logseq.App.showMsg('Hello, LogSeq!');
-    }
-  });
+  logseq.Editor.registerSlashCommand(
+    'Show Block in Message',
+    async () => {
+      const block = await logseq.Editor.getCurrentBlock();
+
+      if (block) {
+        const { content, uuid } = block;
+        logseq.UI.showMsg(`
+          [:div.p-2
+            [:h1 "#${uuid}"]
+            [:h2.text-xl "${content}"]]
+        `)
+      } else {
+        logseq.UI.showMsg('No block selected');
+      }
+    },
+  )
+
+  logseq.Editor.registerSlashCommand(
+    'Append to Block',
+    async () => {
+      const block = await logseq.Editor.getCurrentBlock();
+
+      if (block) {
+        const { content, uuid } = block;
+        // Append "Hello, LogSeq!" in a new block as a child of the current block
+        let insertedBlock = await logseq.Editor.insertBlock(
+          block.uuid, `Hello, ${content}!`);
+          
+        
+      } else {
+        logseq.UI.showMsg('No block selected');
+      }
+    },
+  )
+
 }
 logseq.ready(main).catch(console.error);
